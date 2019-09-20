1259d7dea57cfedf72c720dfaba0ed03f5b3b1a9
audioryan9 <ryandsgn@outlook.com>
2019-09-20 22:39:23 +0000

    asdasd

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,32 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  // from here
+
+  //console.log(Math.floor(Math.random() * group.length));
+
+  let who = ["santa", "a rabbi", "this bbw chick"];
+  let action = "held me ransom";
+  let what = ["with a knife", "in a shrine"];
+  let when = ["today", "in october", "during the eclipse"];
+
+  let whoIndex = 0;
+  let actionIndex = 0;
+  let whatIndex = 0;
+  let whenIndex = 0;
+
+  let generateExcuse = () => {
+    return (
+      who[whoIndex] +
+      "" +
+      action[actionIndex] +
+      "" +
+      [whatIndex] +
+      "" +
+      [whenIndex]
+    );
+  };
+
+  document.body.querySelector("#excuses").innerHTML = "Hello world";
+  // to here
 };
