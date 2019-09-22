74a320e424206c5b8d0e8f7fa76fcb7bbc30940d
audioryan9 <ryandsgn@outlook.com>
2019-09-22 20:20:14 +0000

    asdasd

--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,32 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
+  document.querySelector("#genbttn").addEventListener("click", () => {
+    document.body.querySelector("#excuses").innerHTML = generateExcuse();
+    // GENERATE RANDOM #
+    // USE RANDOM # AS index
+    whoIndex = Math.floor(Math.random() * who.length);
+    actionIndex = Math.floor(Math.random() * action.length);
+    whatIndex = Math.floor(Math.random() * what.length);
+    // whenIndex = Math.floor(Math.random() * when.length);
+
+    let randomnumber = Math.random();
+    console.log(randomnumber);
+    let lengthofwhen = when.length;
+    console.log(lengthofwhen);
+    let rawnumber = randomnumber * lengthofwhen;
+    console.log(rawnumber);
+    let whenIndex = Math.floor(rawnumber);
+    console.log(whenIndex);
+  });
   // from here
 
-  //console.log(Math.floor(Math.random() * group.length));
+  //      create a set of random excuses
+  //      index escuse with random #
+  //      print excuse to DOM
 
   let who = ["santa", "a rabbi", "this bbw chick"];
-  let action = "held me ransom";
+  let action = ["held me ransom"];
   let what = ["with a knife", "in a shrine"];
   let when = ["today", "in october", "during the eclipse"];
 
@@ -24,15 +44,14 @@ window.onload = function() {
   let generateExcuse = () => {
     return (
       who[whoIndex] +
-      "" +
+      " " +
       action[actionIndex] +
-      "" +
-      [whatIndex] +
-      "" +
-      [whenIndex]
+      " " +
+      what[whatIndex] +
+      " " +
+      when[whenIndex]
     );
   };
 
-  document.body.querySelector("#excuses").innerHTML = "Hello world";
   // to here
 };
